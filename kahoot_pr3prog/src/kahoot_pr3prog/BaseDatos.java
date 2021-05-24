59972ab93934d2ed398eae3fea3857c0baefdb22
David <d.salgado.9@gmail.com>
2021-05-24 18:24:29 +0200

    Juego hecho, falta puntos totales y toda la parte de edicion

--- kahoot_pr3prog/src/kahoot_pr3prog/BaseDatos.java
+++ kahoot_pr3prog/src/kahoot_pr3prog/BaseDatos.java
@@ -2,34 +2,95 @@ package kahoot_pr3prog;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BaseDatos {
 
+	private Connection conexion = null;
+	private Statement sentenciaSQL = null;
+	private ResultSet rs;
 		
 	public BaseDatos() {  //Genero el constructor vacio para poder crear desde el programa el objeto bd con el que acceder a todos los metodos
 		super();
 		
 	}
 
-	public void ConectarBaseDatos() throws ClassNotFoundException {
+	public void MostrarTextoBD(int id, boolean EsPregunta) throws ClassNotFoundException, SQLException {
 		
 		try {
+			
+			String sql;
+			
+			if(EsPregunta) {
+				
+				sql="SELECT * FROM preguntas WHERE id_preg="+id;
+				
+			}else {	
+				
+				sql="SELECT * FROM respuestas WHERE id_resp="+id;	
+			}
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost",
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost/preguntas_respuestas",
 					"root", "DSE260403");
 
-			conexion.close();
+			sentenciaSQL = conexion.createStatement();
+			
+			rs=sentenciaSQL.executeQuery(sql);
+			
+			while (rs.next()) {
+				
+				System.out.println(rs.getString(2));
+			}
+			
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 			//System.out.println("Error");
 		}finally {
-		
-			
-			System.out.println("Conectado/desconectado");
-		
+			conexion.close();
+			sentenciaSQL.close();
+					
 		}
 
+	}
+	
+	public int idRespuestaCorrecta(int idpreg) throws ClassNotFoundException, SQLException {
+		
+		int id=0;
+		
+			try {
+				
+				String sql;
+					
+				sql="SELECT cod_resp FROM preguntas WHERE id_preg="+idpreg;
+					
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost/preguntas_respuestas",
+						"root", "DSE260403");
+		
+				sentenciaSQL = conexion.createStatement();
+					
+				rs=sentenciaSQL.executeQuery(sql);
+					
+				while (rs.next()) {
+						
+					id=rs.getInt(1);
+				}
+					
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+				//System.out.println("Error");
+			}finally {
+				conexion.close();
+				sentenciaSQL.close();
+				
+			
+			}
+					
+		
+		
+		return id;
 	}
 		
 	
--- kahoot_pr3prog/src/kahoot_pr3prog/Programa.java
+++ kahoot_pr3prog/src/kahoot_pr3prog/Programa.java
@@ -1,19 +1,29 @@
 package kahoot_pr3prog;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Programa {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 
 		
 		int opcion=0;
 		int opcion1=0;
+		int idpreg;
+		int idcorrecto;
+		int puntos=0;
+		Double aleatorio;
 		String [] menu = {"Jugar","Editar"};
 		String [] menu1 = {"Añadir","Modificar","Eliminar"};
 		String [] menu2 = {"Preguntas","Respuestas"};
 		String [] cont = {"Si","No"};
+		ArrayList <Integer> idpreguntas;
+		ArrayList <Integer> idrespuestas;
 		BaseDatos bd = new BaseDatos();
 		
 		//bd.ConectarBaseDatos();
@@ -24,6 +34,41 @@ public class Programa {
 		opcion=elegirOpcion(menu);
 		
 		if(opcion==1) {
+			
+			do {
+				
+				idpreguntas = new ArrayList <Integer> (generarIDsAleatorio(10,5)); //Convierto el Hash en un ArrayList para poder acceder a las posiciones
+				
+				for(int i=0; i<idpreguntas.size();i++) {
+					
+					idpreg=idpreguntas.get(i);
+					
+					bd.MostrarTextoBD(idpreg, true);
+					
+					idcorrecto=bd.idRespuestaCorrecta(idpreg);					
+					
+					idrespuestas = new ArrayList <Integer> (generarIDsAleatorio(15,3)); //Genero una lista aleatoria de 3 respuestas
+					
+					if(idrespuestas.contains(idcorrecto)) { //Reviso si en las 3 respuestas generadas aleatoriamente está la correcta
+						
+						idrespuestas.add(11); //Es una respuesta que para todas las preguntas es incorrecta,la añado si aleatoriamente se habia incluido la correcta en la lista.
+						
+					}else {
+						
+						idrespuestas.add(idcorrecto);
+											
+					}
+					
+					Collections.shuffle(idrespuestas); //Mezclo las respuestas, por si he añadido la correcta que no esté siempre al final
+					
+					puntos=puntos+responderJuego(idrespuestas,idcorrecto);
+					
+					
+				}
+				
+				System.out.println("¿Quiere volver a jugar?");
+				
+			}while(elegirOpcion(cont)==1);
 			
 		}else {
 			
@@ -125,4 +170,78 @@ public class Programa {
 				
 		return numero;
 	}
+	
+	public static HashSet<Integer> generarIDsAleatorio (int total, int cantidad) {//Genero aleatoriamente un HashSet de X numeros, al no repetirse los valores en esta estructura de almacenamiento, se que no se me van a repetir las preguntas
+		
+		Double aleatorio;
+		
+		HashSet<Integer> preg = new HashSet<Integer>();
+		
+		while(preg.size()<cantidad) {    
+			
+			aleatorio= Math.random()*total + 1;
+			
+			preg.add(aleatorio.intValue());
+			
+		}
+		
+		return preg;
+	}
+	
+	
+	public static int responderJuego(ArrayList<Integer> IDsRespuestas, int idcorrecto) throws ClassNotFoundException, SQLException, InputMismatchException {
+		
+		int idresp;
+		int respuestaCorrecta=0;
+		int respuesta=0;
+		int contador=0;
+		Scanner sc= new Scanner (System.in);
+		BaseDatos bd = new BaseDatos();
+		
+		System.out.println("Opciones:");
+		
+		for(int i=0; i<IDsRespuestas.size();i++) {
+			
+			idresp=IDsRespuestas.get(i);
+			
+			System.out.println(i+1 +":");
+			
+			bd.MostrarTextoBD(idresp, false);
+			
+			if(idresp==idcorrecto) {
+				
+				respuestaCorrecta=i+1;
+			}
+			
+		}
+		
+		try {
+		
+			System.out.println("Responda:");
+			
+			respuesta=sc.nextInt();
+			
+		}catch(Exception e) {
+			
+			System.out.println("Su respuesta no se ajusta a los parametros, se considera un error");		
+			
+			
+		}finally {
+			
+			if(respuesta==respuestaCorrecta) {
+				
+				System.out.println("¡Correcto! Bien hecho");
+				
+				contador=1;
+			}else {
+				
+				System.out.println("Vaya, has fallado");
+			}
+			
+		}
+		
+		return contador;
+		
+	}
+	
 }
